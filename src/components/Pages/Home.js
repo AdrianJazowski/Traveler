9329e42110f6c7f1302aff188475a1bb46caf3ee
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-10-17 20:26:19 +0200

    add footer and css for footer, visual changes in other files

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css'
 import {HeroSection} from '../HeroSection/HeroSection'
 import {Cards} from '../Cards/Cards'
+import { Footer } from '../Footer/Footer';
 
 export const Home = () =>{
     return(
         <>
             <HeroSection/>
             <Cards/>
+            <Footer/>
         </>
     )
 }
